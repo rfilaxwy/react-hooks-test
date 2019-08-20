ac0b6ad34bc3249f8c216d720b2cf1aa7b6b285f
Ryan <ryanfilaxwylie@gmail.com>
2019-08-20 06:34:10 -0600

    fixed css issue re: media query not working

--- re-test/src/components/NewTask.js
+++ re-test/src/components/NewTask.js
@@ -4,7 +4,7 @@ import { FaTimes, FaCheck } from 'react-icons/fa';
 
 const NewTask = (props) => {
     
-    let taskCompleteStyle = {textDecoration: "line-through"};
+    let taskCompleteStyle = {textDecoration: "line-through", color:'rgb(222,222,222)'};
     let taskOutput = props.complete ? <p className={styles.task} style={taskCompleteStyle}>{props.taskName}</p>: <p className={styles.task}>{props.taskName}</p>
     let checkBox = props.complete ? <div className={styles.checkBlock} onClick={()=>props.completeTask(props.id)}><FaCheck /></div>:<div className={styles.checkBlock} type="checkbox" onClick={()=>props.completeTask(props.id)}/>
     return(
