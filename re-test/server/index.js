890f6221537c590789e796c2e5b2ae010e9c99f6
Ryan <ryanfilaxwylie@gmail.com>
2019-08-24 11:23:23 -0600

    Worked on server

--- re-test/server/index.js
+++ re-test/server/index.js
@@ -1,17 +1,33 @@
-const express = require('express'),
-    bodyParser = require('body-parser'),
-    cors = require('cors'),
-    app = express()
+const   express = require('express'),
+        bodyParser = require('body-parser'),
+        cors = require('cors'),
+        app = express(),
+        path = require('path'),
+        axios = require('axios')
     // massive = require('massive') for later when attach this to a server
+
 
 require('dotenv').config();
 app.use(cors());
 app.use(bodyParser.json());
 
 
-let weathapi = process.env.WEATHERAPI
-let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&APPID=${weathapi}`
+const weathapi = process.env.WEATHERAPI
+const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&APPID=${weathapi}`
+const port = process.env.PORT || 8888;
 
-app.get('/weather', weatherUrl).then((res) =>{
-    res.send(res)
+app.listen(port);
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+})
+
+app.get('/weather', (req, res)=>{
+    let data;
+    axios.get(weatherUrl).then(res=>{
+        data = res.data ? res.data.main.temp:null;
+    })
+    res.send(data)
 }).catch(err=> err.send (new Error('Could not find local weather',{status:500})))
--- re-test/src/components/WeatherContainer/WeatherContainer.js
+++ re-test/src/components/WeatherContainer/WeatherContainer.js
@@ -5,22 +5,27 @@ import classes from './WeatherContainer.module.css';
 require('dotenv').config();
 
 export default class WeatherContainer extends Component{
-    constructor(){
-        super()
-        this.state = {
-            weatherCels: null
-        }
+    constructor(props){
+        super(props)
+        
+    }
+
+    state = {
+        weatherCels: null
+    }
+
+    static getDerivedStateFromProps(props, state){
+        return state;
     }
 
     componentDidMount(){
         // axios.post('url of the cloudflare worker', {request.body})
-        // let weathapi = process.env.WEATHERAPI
+        let weathapi = process.env.WEATHERAPI
         // let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&APPID=${weathapi}` 
         let localWeather;
-        axios.get('/weather').then((res)=>{
-            console.log(res)
-           localWeather = res.data.weather
-           this.setState({
+        axios.get(`).then((res)=>{
+            localWeather = Math.floor(res.data.main.temp - 273)
+            this.setState({
             weather: localWeather
         })
         })
@@ -33,7 +38,7 @@ export default class WeatherContainer extends Component{
         let weatherDisplay = this.state.weatherCels === null ? 'offline':this.state.weather;
         return(
             <div className={classes.weatherBar}>
-                <p>{weatherDisplay}</p>
+                <p>{weatherDisplay} celsius</p>
             </div>
         )
     }
